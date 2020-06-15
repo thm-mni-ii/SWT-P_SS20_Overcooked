4ef57da010d445e35ed4aa8b9eb56ddecec13e46
pantAlex <banpa@web.de>
2020-06-15 20:05:54 +0200

    Implement dropping objects once picked up + use raycasting when interacting with objects

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
 public class PickupItem : NetworkBehaviour
 {
-    bool isPickedUp = false;
+    [SerializeField] float pickupReach = 0.5F;
+    [SerializeField] LayerMask pickupLayers;
+    [SerializeField] Transform reachOrigin;
+
+
     GameObject interactables;
     GameObject pickup;
     GameObject player;
@@ -24,36 +28,41 @@ public class PickupItem : NetworkBehaviour
         {
             if (isLocalPlayer)
             {
-                if (!isPickedUp && pickup != null)
-                {
-                    pickup.transform.SetParent(player.transform);
-                    //pickup.transform.position = pickup.gameObject.transform.position + new Vector3(0, 0, 0);
-                    isPickedUp = true;
-                }
-                /*if (isPickedUp)
+                if (pickup != null)
                 {
                     pickup.transform.SetParent(interactables.transform);
-                    isPickedUp = false;
-                }*/
-            }            
+                    pickup.GetComponent<Rigidbody>().isKinematic = false;
+                    pickup = null;
+                }
+                else
+                {
+                    RaycastHit hitInfo;
+                    if (Physics.Raycast(reachOrigin.position, reachOrigin.forward, out hitInfo, pickupReach, pickupLayers))
+                    {
+                        pickup = hitInfo.collider.gameObject;
+                        pickup.transform.SetParent(player.transform);
+                        pickup.GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                }
+            }
         }
-        
+
     }
 
-    private void OnTriggerEnter(Collider collider) 
-    {
-        if (collider.gameObject.tag == "Pickup")
-        {
-            pickup = collider.gameObject;
-           
-        }
-    }
-    private void OnTriggerExit(Collider collider)
-    {
-        if (collider.gameObject.tag == "Pickup")
-        {
-            pickup = null;
+    // private void OnTriggerEnter(Collider collider) 
+    // {
+    //     if (collider.gameObject.tag == "Pickup")
+    //     {
+    //         pickup = collider.gameObject;
 
-        }
-    }
+    //     }
+    // }
+    // private void OnTriggerExit(Collider collider)
+    // {
+    //     if (collider.gameObject.tag == "Pickup")
+    //     {
+    //         pickup = null;
+
+    //     }
+    // }
 }
