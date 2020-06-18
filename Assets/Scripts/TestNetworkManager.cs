2c960e667b817bc2c764390c279c7010ccd55c8e
CHollatz <christian.mike.hollatz@mni.thm.de>
2020-06-18 11:12:19 +0200

    fixed spawn points, added additional spawns for up to 4 players

--- Assets/Scripts/TestNetworkManager.cs
+++ Assets/Scripts/TestNetworkManager.cs
@@ -6,14 +6,17 @@ using TMPro;
 
 public class TestNetworkManager : NetworkManager
 {
-    [SerializeField] Transform spawnPoint;
+    [SerializeField] Transform spawnPoint1;
+    [SerializeField] Transform spawnPoint2;
+    [SerializeField] Transform spawnPoint3;
+    [SerializeField] Transform spawnPoint4;
     [SerializeField] Timer timer;
+    Transform spawn;
 
 
     public override void OnStartServer()
     {
         this.timer.ShowControls();
-        NetworkServer.RegisterHandler<AddPlayerMessage>(this.SpawnClient);
     }
     public override void OnStopServer()
     {
@@ -23,12 +26,28 @@ public class TestNetworkManager : NetworkManager
     {
         base.OnClientConnect(conn);
         conn.Send(new AddPlayerMessage());
+
     }
 
-
-    private void SpawnClient(NetworkConnection conn, AddPlayerMessage message)
+    public override void OnServerAddPlayer(NetworkConnection conn)
     {
-        GameObject playerGO = GameObject.Instantiate(this.playerPrefab, this.spawnPoint.position, this.spawnPoint.rotation);
+        switch (numPlayers)
+        {
+            case 0:
+                spawn = spawnPoint1;
+                break;
+            case 1:
+                spawn = spawnPoint2;
+                break;
+            case 2:
+                spawn = spawnPoint3;
+                break;
+            case 3:
+                spawn = spawnPoint4;
+                break;
+
+        }
+        GameObject playerGO = GameObject.Instantiate(this.playerPrefab, this.spawn.position, this.spawn.rotation);
         NetworkServer.AddPlayerForConnection(conn, playerGO);
     }
 }
