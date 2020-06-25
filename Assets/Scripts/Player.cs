0e0466f984bc3b5f1e80f60490e8bbef5cca9837
pantAlex <banpa@web.de>
2020-06-25 13:19:20 +0200

    Add player class + Fix player picking up multiple objects

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] Interactor interactor;
+    [SerializeField] PlayerControls controls;
+
+
+    public PickableObject HeldObject => this.interactor.HeldObject;
+    public bool IsHoldingObject => this.interactor.IsHoldingObject;
+
+
+    #region Unity Callbacks
+
+    private void Update()
+    {
+        if (this.isLocalPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                this.interactor.Interact();
+        }
+    }
+
+    #endregion
+}
