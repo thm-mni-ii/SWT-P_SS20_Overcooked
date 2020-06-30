64a15c8f97467766647512a332b6c3fd727a84d8
pantAlex <banpa@web.de>
2020-06-30 13:33:06 +0200

    Start working on GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+
+
+    public Level CurrentLevel { get; private set; }
+
+
+
+    private void Awake()
+    {
+        if (GameManager.Instance == null)
+        {
+            GameManager.Instance = this;
+            this.CurrentLevel = null;
+
+            // TODO: Load player data
+            // TODO: Check startup parameters & connect to given server
+        }
+        else
+            GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] int levelDurationSeconds = 180;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
