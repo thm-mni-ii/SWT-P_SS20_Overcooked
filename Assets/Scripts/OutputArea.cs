ab63c9e0d49a968e3060e3fe00bef330759f0b49
pantAlex <banpa@web.de>
2020-07-02 12:05:18 +0200

    Implement output area

---
+++ Assets/Scripts/OutputArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class OutputArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        ElementObject elementObject = other.gameObject.GetComponent<ElementObject>();
+
+        if (elementObject != null && this.CanAccept(elementObject.Element))
+            NetworkServer.Destroy(other.gameObject);
+    }
+
+
+    private bool CanAccept(Recipe recipe)
+    {
+        foreach (Recipe r in GameManager.Instance.GameDemandQueue.CurrentDemands)
+            if (recipe.Equals(r))
+                return true;
+
+        return false;
+    }
+}
