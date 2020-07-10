ddaf9621a151a8e3dcb544d683a9516db4761f54
pantAlex <banpa@web.de>
2020-07-10 22:29:59 +0200

    Implement player score, increasing when delivering an item from the demand queue

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,19 +6,24 @@ using Mirror;
 public class Level : NetworkBehaviour
 {
     [Header("Settings")]
+    [SerializeField] int scorePerDelivery = 50;
     [SerializeField] int levelDurationSeconds = 180;
     [SerializeField] Matter[] demandsPool;
     [SerializeField] Transform[] spawnPoints;
 
 
+    public int PlayerScore => this.PlayerScore;
     public Transform[] SpawnPoints => this.spawnPoints;
 
+    [SyncVar] int playerScore;
     private Coroutine demandCoroutine;
     private WaitForSeconds demandCoroutineWait;
 
 
     private void Awake()
     {
+        this.playerScore = 0;
+        this.demandCoroutine = null;
         this.demandCoroutineWait = new WaitForSeconds(5.0F);
     }
 
@@ -46,10 +51,18 @@ public class Level : NetworkBehaviour
         {
             if (GameManager.UI.LevelUI.DemandQueue.HasDemand(matter))
             {
+                this.IncrementPlayerScore(this.scorePerDelivery);
                 GameManager.UI.LevelUI.DemandQueue.DeliverDemand(matter);
                 NetworkServer.Destroy(elementObject.gameObject);
             }
         }
+    }
+
+    public void IncrementPlayerScore(int scoreDelta) => this.SetPlayerScore(this.playerScore + scoreDelta);
+    public void SetPlayerScore(int newScore)
+    {
+        if (this.isServer)
+            this.playerScore = newScore;
     }
 
 
