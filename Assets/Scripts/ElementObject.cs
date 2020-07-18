120f27419eb7a1cf07b65aea71af0654729bf224
pantAlex <banpa@web.de>
2020-07-18 23:49:21 +0200

    Disable physics on picked up matter objects and re-enable it when dropped

--- Assets/Scripts/ElementObject.cs
+++ Assets/Scripts/ElementObject.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PickableObject))]
 public class ElementObject : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] Rigidbody[] rigidbodies;
+    [SerializeField] Collider[] colliders;
+    [SerializeField] PickableObject pickableObject;
 
     [Header("Settings")]
     [SerializeField] Matter element;
@@ -14,14 +17,30 @@ public class ElementObject : MonoBehaviour
     public Matter Element => this.element;
 
 
+    private void Awake()
+    {
+        // Setup events for reacting to picking up and dropping this element
+        this.pickableObject.OnPickedUp += this.OnPickedUp;
+        this.pickableObject.OnDropped += this.OnDropped;
+    }
+
+
     public void EnablePhysics()
     {
         foreach (Rigidbody rb in this.rigidbodies)
             rb.isKinematic = false;
+        foreach (Collider c in this.colliders)
+            c.enabled = true;
     }
     public void DisablePhysics()
     {
         foreach (Rigidbody rb in this.rigidbodies)
             rb.isKinematic = true;
+        foreach (Collider c in this.colliders)
+            c.enabled = false;
     }
+
+
+    private void OnPickedUp(PickableObject pickedUp, Interactor picker) => this.DisablePhysics();
+    private void OnDropped(PickableObject dropped, Interactor dropper) => this.EnablePhysics();
 }
