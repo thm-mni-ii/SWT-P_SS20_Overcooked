50a9b49ddec73600fa7ef5f2fbff4dcca5d89f68
pantAlex <banpa@web.de>
2020-08-04 16:48:26 +0200

    Add documentation for UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages the game UI.
+/// Contains references to all UI elements.
+/// </summary>
 public class UIManager : MonoBehaviour
 {
     [Header("References")]
+    [Tooltip("A reference to the UI canvas.")]
     [SerializeField] Canvas uiCanvas;
+    [Tooltip("A reference to the level UI.")]
     [SerializeField] LevelUI levelUI;
 
 
+    /// <summary>
+    /// A reference to the UI canvas.
+    /// </summary>
     public Canvas UICanvas => this.uiCanvas;
+    /// <summary>
+    /// A reference to the level UI.
+    /// </summary>
     public LevelUI LevelUI => this.levelUI;
 }
