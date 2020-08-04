bd764d4180a7c292240179ede7d974854e2df33d
pantAlex <banpa@web.de>
2020-08-04 15:48:29 +0200

    Add documentation to recipes

--- Assets/Scripts/ScriptableObjects/Recipes/RecipeMatchState.cs
+++ Assets/Scripts/ScriptableObjects/Recipes/RecipeMatchState.cs
@@ -1,6 +1,18 @@
+/// <summary>
+/// Tells how much of a recipe has been matched.
+/// </summary>
 public enum RecipeMatchState
 {
+    /// <summary>
+    /// The recipe has not been matched at all (there are foreign elements which are not allowed).
+    /// </summary>
     NoMatch,
+    /// <summary>
+    /// The recipe has been partially matched (there are a few missing elements).
+    /// </summary>
     PartialMatch,
+    /// <summary>
+    /// The recipe has been fully matched and can produce its output.
+    /// </summary>
     FullMatch
 }
