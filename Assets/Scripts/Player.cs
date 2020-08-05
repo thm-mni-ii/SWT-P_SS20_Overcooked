c626057ced4055c3274f537915771ccf3c3fe2a2
JoeGre666 <johannes.greising@mni.thm.de>
2020-08-05 08:26:22 +0200

    Add documentation for Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,27 +5,40 @@ using Mirror;
 
 namespace Underconnected
 {
+    /// <summary>
+    /// Will check if local player presses the 'E' key and if so calls Interact.
+    /// </summary>
     public class Player : NetworkBehaviour
     {
         [SerializeField] Interactor interactor;
         [SerializeField] PlayerControls controls;
 
-
+        /// <summary>
+        /// the held object of local player
+        /// </summary>
         public PickableObject HeldObject => this.interactor.HeldObject;
+        /// <summary>
+        /// checks if player is holding an object
+        /// </summary>
         public bool IsHoldingObject => this.interactor.IsHoldingObject;
 
 
         #region Unity Callbacks
 
+        /// <summary>
+        /// Will check if local player presses the 'E' key and if so calls Interact.
+        /// </summary>
         private void Update()
         {
             if (this.isLocalPlayer)
             {
-                if (Input.GetKeyDown(KeyCode.E))
-                    this.interactor.Interact();
+                if (this.isLocalPlayer)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                        this.interactor.Interact();
+                }
             }
         }
-
         #endregion
     }
 }
