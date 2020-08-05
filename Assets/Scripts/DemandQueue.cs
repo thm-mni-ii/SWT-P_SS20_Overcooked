1f4ea67d72188dfddb7beca7dceda54678ab6704
JoeGre666 <johannes.greising@mni.thm.de>
2020-08-05 21:09:03 +0200

    Add documentation for DemandQueue.

--- Assets/Scripts/DemandQueue.cs
+++ Assets/Scripts/DemandQueue.cs
@@ -5,12 +5,17 @@ using Mirror;
 
 namespace Underconnected
 {
+    /// <summary>
+    /// Manages all functionalities concerning the queue of demanded matters like adding, removing and delivering.
+    /// </summary>
     public class DemandQueue : NetworkBehaviour
     {
         [SerializeField] GameObject queueElementsContainer;
         [SerializeField] GameObject demandedMatterUIPrefab;
 
-
+        /// <summary>
+        /// List of matter demands
+        /// </summary>
         private List<Demand> currentDemands;
 
 
@@ -19,7 +24,11 @@ namespace Underconnected
             this.currentDemands = new List<Demand>();
         }
 
-
+        /// <summary>
+        /// Adds a matter to demand queue with <see cref="RpcAcceptDemand(string)"/> for server
+        /// or <see cref="AcceptDemand(Matter)"/> for clients.
+        /// </summary>
+        /// <param name="matter">matter to be added to demand queue</param>
         public void AddDemand(Matter matter)
         {
             if (this.isServer)
@@ -27,6 +36,12 @@ namespace Underconnected
             else
                 this.AcceptDemand(matter);
         }
+        /// <summary>
+        /// Checks if given matter is currently demanded and 
+        /// if so removes matter from queue with <see cref="RpcRemoveDemand(string)"/> for server
+        /// and <see cref="RemoveDemand(Matter)"/> for clients.
+        /// </summary>
+        /// <param name="matter"></param>
         public void DeliverDemand(Matter matter)
         {
             if (this.HasDemand(matter))
@@ -37,7 +52,11 @@ namespace Underconnected
                     this.RemoveDemand(matter);
             }
         }
-
+        /// <summary>
+        /// Checks if given matter is currently demanded.
+        /// </summary>
+        /// <param name="matter">given matter to be checked</param>
+        /// <returns>true if given matter is demanded or false if not</returns>
         public bool HasDemand(Matter matter)
         {
             if (matter != null)
@@ -50,7 +69,10 @@ namespace Underconnected
             return false;
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="matter"></param>
         private void AcceptDemand(Matter matter)
         {
             GameObject uiElement = GameObject.Instantiate(this.demandedMatterUIPrefab, Vector3.zero, Quaternion.identity, this.queueElementsContainer.transform);
@@ -59,6 +81,12 @@ namespace Underconnected
             demandedMatterUI?.SetMatter(matter);
             this.currentDemands.Add(new Demand(matter, demandedMatterUI));
         }
+        /// <summary>
+        /// Is only called after checking if given matter is currently demanded.
+        /// Iterates through queue and removes first instance of given matter
+        /// from queue and UI element.
+        /// </summary>
+        /// <param name="matter">matter to be removed from demand queue</param>
         private void RemoveDemand(Matter matter)
         {
             for (int i = 0; i < this.currentDemands.Count; i++)
@@ -74,6 +102,11 @@ namespace Underconnected
 
 
         [ClientRpc]
+        /// <summary>
+        /// Is only called by server.
+        /// Checks if a matter with given ID exists and then calls <see cref="AcceptDemand(Matter)"/>.
+        /// </summary>
+        /// <param name="matterID">id of matter to be accepted to queue</param>
         private void RpcAcceptDemand(string matterID)
         {
             Matter targetMatter = Matter.GetByID(matterID);
@@ -83,6 +116,11 @@ namespace Underconnected
                 Debug.LogError($"Cannot accept nonexisting matter with id '{matterID}'.");
         }
         [ClientRpc]
+        /// <summary>
+        /// Is only called by server and after checking if given matter is currently demanded.
+        /// Checks if a matter with given ID exists and then calls <see cref="RemoveDemand(Matter)"/>.
+        /// </summary>
+        /// <param name="matterID">id of matter to be removed from queue</param>
         private void RpcRemoveDemand(string matterID)
         {
             Matter targetMatter = Matter.GetByID(matterID);
@@ -93,7 +131,9 @@ namespace Underconnected
         }
 
 
-
+        /// <summary>
+        /// A Demand struct which contains the demanded matter and its uiElement.
+        /// </summary>
         public struct Demand
         {
             public Matter demandedMatter;
