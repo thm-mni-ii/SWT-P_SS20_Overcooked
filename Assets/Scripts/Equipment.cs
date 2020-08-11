bfb61217172504d3eff8d5a0cad468c2877dd94a
pantAlex <banpa@web.de>
2020-08-11 10:50:23 +0200

    Add documentation for Equipment

--- Assets/Scripts/Equipment.cs
+++ Assets/Scripts/Equipment.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 namespace Underconnected
 {
+    /// <summary>
+    /// Represents a piece of lab equipment that can be interacted with.
+    /// </summary>
     public class Equipment : ModifiableObject
     {
         [Header("References")]
@@ -18,9 +21,22 @@ namespace Underconnected
         [SerializeField] Recipe[] allRecipes;
 
 
+        /// <summary>
+        /// Stores all the inserted matter objects.
+        /// </summary>
         private List<MatterObject> insertedObjects;
+        /// <summary>
+        /// Stores all the inserted matter objects from <see cref="insertedObjects"/> as simple matters.
+        /// Used inside of <see cref="InsertMatterObject(MatterObject)"/> to determine which recipe can be made out of the contained matters.
+        /// </summary>
         private List<Matter> insertedMatter;
+        /// <summary>
+        /// The recipe that is currently being processed by this equipment.
+        /// </summary>
         private Recipe recipeInProgress;
+        /// <summary>
+        /// The output slot of this equipment.
+        /// </summary>
         private MatterObject outputObject;
 
 
@@ -33,6 +49,12 @@ namespace Underconnected
         }
 
 
+        /// <summary>
+        /// Interacts with this object.
+        /// Attempts to insert a matter object held by <paramref name="interactor"/> or take a matter object from the output slot.
+        /// If the interactor does not hold anything and the output slot is empty, this method will try take the last inserted object and give it to the interactor.
+        /// </summary>
+        /// <param name="interactor">The initiator of this interaction.</param>
         public override void Interact(Interactor interactor)
         {
             if (this.IsActivated)
@@ -63,6 +85,10 @@ namespace Underconnected
         }
 
 
+        /// <summary>
+        /// Called when the timer has finished.
+        /// Disables all effects, hides the progress bar and calls <see cref="CompleteRecipe(Recipe)"/>.
+        /// </summary>
         protected override void OnTimerFinish()
         {
             if (!this.IsFinished)
@@ -80,6 +106,11 @@ namespace Underconnected
         }
 
 
+        /// <summary>
+        /// Inserts the given matter object into this equipment.
+        /// Checks whether a recipe matches the inserted elements and starts the production process, shows the progress bar and enables effects if a matching recipe has been found.
+        /// </summary>
+        /// <param name="matterObject"></param>
         private void InsertMatterObject(MatterObject matterObject)
         {
             if (matterObject != null)
@@ -110,6 +141,10 @@ namespace Underconnected
                 }
             }
         }
+        /// <summary>
+        /// Ejects the matter object inside the output slot and returns it.
+        /// </summary>
+        /// <returns>The output matter object or `null` if none.</returns>
         private MatterObject TakeOutputObject()
         {
             if (this.outputObject != null)
@@ -124,6 +159,10 @@ namespace Underconnected
 
             return null;
         }
+        /// <summary>
+        /// Ejects the last inserted matter object, removes it from the input and returns it.
+        /// </summary>
+        /// <returns>The last inserted matter object or `null` if none.</returns>
         private MatterObject TakeLastInsertedObject()
         {
             if (this.insertedObjects.Count > 0)
@@ -142,20 +181,31 @@ namespace Underconnected
             return null;
         }
 
+        /// <summary>
+        /// Finishes the given recipe and sends the results to the clients.
+        /// Removes matters from the input that have been consumed by the recipe.
+        /// Adds the result matter to the output slot.
+        /// Can only be called on a server.
+        /// </summary>
+        /// <param name="recipe">The finished recipe.</param>
+        [Server]
         private void CompleteRecipe(Recipe recipe)
         {
-            if (this.isServer)
-            {
-                this.RemoveFromInput(recipe.Inputs);
-                this.AddToOutput(recipe.Output);
+            this.RemoveFromInput(recipe.Inputs);
+            this.AddToOutput(recipe.Output);
 
-                this.recipeInProgress = null;
-                this.RpcCompleteRecipe();
-            }
+            this.recipeInProgress = null;
+            this.RpcCompleteRecipe();
         }
+        /// <summary>
+        /// Adds the given matter to the output slot and synchronizes it with the clients.
+        /// Can only be called on the server.
+        /// </summary>
+        /// <param name="matter">The matter to add.</param>
+        [Server]
         private void AddToOutput(Matter matter)
         {
-            if (this.isServer && matter != null)
+            if (matter != null)
             {
                 GameObject resultMatter = GameObject.Instantiate(matter.GetPrefab());
 
@@ -170,36 +220,48 @@ namespace Underconnected
                 this.RpcAddOutput(resultMatter.GetComponent<NetworkIdentity>());
             }
         }
+        /// <summary>
+        /// Removes the given matters from the input and synchronizes it with the clients.
+        /// Can only be called on the server.
+        /// </summary>
+        /// <param name="matters">The matters to remove from the input.</param>
+        [Server]
         private void RemoveFromInput(Matter[] matters)
         {
-            if (this.isServer)
+            List<int> removeIndices = new List<int>(matters.Length);
+            foreach (Matter toRemove in matters)
             {
-                List<int> removeIndices = new List<int>(matters.Length);
-                foreach (Matter toRemove in matters)
+                int insertedSlotIndex = this.insertedMatter.IndexOf(toRemove);
+                while (insertedSlotIndex >= 0 && removeIndices.Contains(insertedSlotIndex) && insertedSlotIndex + 1 < this.insertedMatter.Count)
+                    insertedSlotIndex = this.insertedMatter.IndexOf(toRemove, insertedSlotIndex + 1);
+
+                if (insertedSlotIndex >= 0 && insertedSlotIndex < this.insertedMatter.Count)
                 {
-                    int insertedSlotIndex = this.insertedMatter.IndexOf(toRemove);
-                    while (insertedSlotIndex >= 0 && removeIndices.Contains(insertedSlotIndex) && insertedSlotIndex + 1 < this.insertedMatter.Count)
-                        insertedSlotIndex = this.insertedMatter.IndexOf(toRemove, insertedSlotIndex + 1);
+                    NetworkServer.Destroy(this.insertedObjects[insertedSlotIndex].gameObject);
 
-                    if (insertedSlotIndex >= 0 && insertedSlotIndex < this.insertedMatter.Count)
-                    {
-                        NetworkServer.Destroy(this.insertedObjects[insertedSlotIndex].gameObject);
-
-                        this.insertedMatter.RemoveAt(insertedSlotIndex);
-                        this.insertedObjects.RemoveAt(insertedSlotIndex);
-                        removeIndices.Add(insertedSlotIndex);
-                    }
+                    this.insertedMatter.RemoveAt(insertedSlotIndex);
+                    this.insertedObjects.RemoveAt(insertedSlotIndex);
+                    removeIndices.Add(insertedSlotIndex);
                 }
-                this.RpcRemoveFromInput(removeIndices.ToArray());
             }
+            this.RpcRemoveFromInput(removeIndices.ToArray());
         }
 
 
+        /// <summary>
+        /// Tells all clients to reset the currently processed recipe.
+        /// Sent by the server to all clients.
+        /// </summary>
         [ClientRpc]
         private void RpcCompleteRecipe()
         {
             this.recipeInProgress = null;
         }
+        /// <summary>
+        /// Tells all clients to remove elements from the input at the given indices.
+        /// Sent by the server to all clients.
+        /// </summary>
+        /// <param name="indices">The element indices to remove.</param>
         [ClientRpc]
         private void RpcRemoveFromInput(int[] indices)
         {
@@ -215,7 +277,7 @@ namespace Underconnected
                 }
                 catch (System.Exception ex)
                 {
-                    Debug.LogError($"Cannot remove given indices from input list. Clearing entire list instead.", this);
+                    Debug.LogError("Cannot remove given indices from input list. Clearing entire list instead.", this);
                     Debug.LogException(ex, this);
 
                     this.insertedObjects.Clear();
@@ -223,6 +285,11 @@ namespace Underconnected
                 }
             }
         }
+        /// <summary>
+        /// Tells all clients to clear the entire input.
+        /// Sent by the server to all clients.
+        /// Currently not used anywhere.
+        /// </summary>
         [ClientRpc]
         private void RpcClearInput()
         {
@@ -232,6 +299,11 @@ namespace Underconnected
                 this.insertedMatter.Clear();
             }
         }
+        /// <summary>
+        /// Tells all clients to assign the given object to the output slot.
+        /// Sent by the server to all clients.
+        /// </summary>
+        /// <param name="outputObject">The <see cref="NetworkIdentity"/> component of the new output object. Expects to have a <see cref="MatterObject"/> component.</param>
         [ClientRpc]
         private void RpcAddOutput(NetworkIdentity outputObject)
         {
