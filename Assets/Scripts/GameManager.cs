9c9c20b38b837161a514ca9009ec2ef03770208b
pantAlex <banpa@web.de>
2020-08-16 15:03:13 +0200

    Fix double-loading the level scene if it is the active one when launching from the Unity Editor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -77,6 +77,17 @@ namespace Underconnected
             else
                 GameObject.Destroy(this.gameObject);
         }
+        private void Start()
+        {
+            // SceneManager does not fire its SceneLoaded event for the currently active scene.
+            // Meaning if we start from inside the Unity Editor and the opened level scene is the active one,
+            // it won't be registered by SceneManager_SceneLoaded.
+            // We need to check manually if the active scene is a level, set it as the current level and fire the OnLevelLoaded event
+            // (basically mimic SceneManager_SceneLoaded's logic).
+            if (this.currentLevel == null)
+                if (this.TrySetCurrentLevelScene(SceneManager.GetActiveScene()))
+                    GameManager.OnLevelLoaded?.Invoke(GameManager.CurrentLevelNum, GameManager.CurrentLevel);
+        }
 
         /// <summary>
         /// Loads the level with the given level number.
@@ -113,18 +124,16 @@ namespace Underconnected
 
 
         /// <summary>
-        /// Called when a scene is loaded by the SceneManager.
-        /// Tries to find a <see cref="Level"/> component on a root game object and sets it as the current level.
-        /// Also fires the <see cref="OnLevelLoaded"/> event.
+        /// Tries to find a <see cref="Level"/> component on the given scene's root game objects and set it as the current level (see <see cref="CurrentLevel"/>).
         /// Has no effect if the loaded scene is not a level.
         /// </summary>
-        /// <param name="scene">The loaded scene.</param>
-        /// <param name="mode">The mode the given <paramref name="scene"/> has been loaded with.</param>
-        private void SceneManager_SceneLoaded(Scene scene, LoadSceneMode mode)
+        /// <param name="levelScene">The scene to try to set as the current level.</param>
+        /// <returns>Whether the given scene contained a level and it was set as the current level.</returns>
+        private bool TrySetCurrentLevelScene(Scene levelScene)
         {
-            if (scene.isLoaded)
+            if (levelScene.isLoaded)
             {
-                GameObject[] rootGOs = scene.GetRootGameObjects();
+                GameObject[] rootGOs = levelScene.GetRootGameObjects();
                 Level levelComponent;
 
                 foreach (GameObject rootGO in rootGOs)
@@ -134,18 +143,33 @@ namespace Underconnected
                     if (levelComponent != null)
                     {
                         this.currentLevel = levelComponent;
-                        this.currentLevelScene = scene;
+                        this.currentLevelScene = levelScene;
 
-                        SceneManager.SetActiveScene(scene);
+                        SceneManager.SetActiveScene(levelScene);
 
-                        Debug.Log($"Level loaded: {this.currentLevel.gameObject.name}");
-                        GameManager.OnLevelLoaded?.Invoke(GameManager.CurrentLevelNum, GameManager.CurrentLevel);
-                        break;
+                        Debug.Log($"Current level is now: {this.currentLevel.gameObject.scene.name}");
+                        return true;
                     }
                 }
             }
             else
-                Debug.LogError($"Scene with index {scene.buildIndex} could not be loaded in time.");
+                Debug.LogError($"Scene with index {levelScene.buildIndex} could not be loaded in time.");
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// Called when a new scene is loaded by the SceneManager.
+        /// Tries to set the loaded scene as the current level and fires the <see cref="OnLevelLoaded"/> event if successful.
+        /// Has no effect if the loaded scene is not a level.
+        /// </summary>
+        /// <param name="scene">The loaded scene.</param>
+        /// <param name="mode">The mode the given <paramref name="scene"/> has been loaded with.</param>
+        private void SceneManager_SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (this.TrySetCurrentLevelScene(scene))
+                GameManager.OnLevelLoaded?.Invoke(GameManager.CurrentLevelNum, GameManager.CurrentLevel);
         }
     }
 }
