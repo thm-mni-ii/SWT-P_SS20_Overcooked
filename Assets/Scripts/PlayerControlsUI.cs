774591eccd8743d16aa7fcf3c4cf0f671d1ebd74
Dominik Horny <dominik.horny@mni.thm.de>
2020-08-24 15:23:34 +0200

    added some changeable keybindings

--- Assets/Scripts/PlayerControlsUI.cs
+++ Assets/Scripts/PlayerControlsUI.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
 
 namespace Underconnected {
     /// <summary>
     /// Represents the "Control Settings Screen" screen that is shown after ...
     /// </summary>
     public class PlayerControlsUI : MonoBehaviour {
+
+        [SerializeField] TextMeshProUGUI forwardButtonText;
+
+        [SerializeField] InputMaster inputMaster;
+
         /// <summary>
         /// Hides this screen.
         /// </summary>
+
+        
         public void Close()
         {
             this.gameObject.SetActive(false);
+        }
+
+        public void ChangeForward(){
+           
+            forwardButtonText.text = "T";
+            inputMaster.Player.Movement.PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .WithCancelingThrough("<Keyboard>/escape")
+            .WithTargetBinding(0)
+            .WithBindingGroup("Keyboard")
+            .OnMatchWaitForAnother(0.1f)
+            .Start();
+            
         }
     }
 }
