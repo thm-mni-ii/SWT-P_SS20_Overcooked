241aa2f7f05f2b6e515fd04742ba0afd12c4eccd
CHollatz <christian.mike.hollatz@mni.thm.de>
2020-08-28 11:08:43 +0200

    added demands, recipes and equipment for level 3

---
+++ Assets/Scripts/RemoveObjectSingleplayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Underconnected
+{
+    public class RemoveObjectSingleplayer : MonoBehaviour
+    {
+        int playerAmount = 1;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(playerAmount == 1)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+
+}
+
