7e5e792ea2c91d394ff886dd885727a2ca0cb512
pantAlex <banpa@web.de>
2020-08-29 17:42:41 +0200

    Add first implementation of ContentsUI and MatterIcon

---
+++ Assets/Scripts/ContentsUI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Underconnected
+{
+    /// <summary>
+    /// Represents a UI element displaying the contents of a game object that contains matter elements.
+    /// </summary>
+    public class ContentsUI : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] MatterIcon matterIconPrefab;
+
+
+        /// <summary>
+        /// Holds the matter that is currently displayed.
+        /// </summary>
+        private Dictionary<Matter, MatterIcon> displayedMatter;
+
+
+        private void Awake()
+        {
+            if (this.displayedMatter == null)
+                this.displayedMatter = new Dictionary<Matter, MatterIcon>();
+        }
+
+
+        /// <summary>
+        /// Adds the given matter to this UI.
+        /// </summary>
+        /// <param name="matter">The matter to add.</param>
+        /// <param name="quantity">The amount of that matter to add (optional, defaults to 1).</param>
+        public void AddMatter(Matter matter, int quantity = 1)
+        {
+            if (matter != null)
+            {
+                MatterIcon matterIcon = null;
+                int newQuantity = quantity;
+
+                if (!this.displayedMatter.ContainsKey(matter))
+                {
+                    GameObject matterIconObject = GameObject.Instantiate(this.matterIconPrefab.gameObject, this.transform);
+                    matterIcon = matterIconObject.GetComponent<MatterIcon>();
+                    this.displayedMatter.Add(matter, matterIcon);
+                }
+                else
+                {
+                    matterIcon = this.displayedMatter[matter];
+                    newQuantity += matterIcon.DisplayedQuantity;
+                }
+
+                matterIcon.SetDisplay(matter, newQuantity);
+            }
+        }
+
+        /// <summary>
+        /// Removes the given matter from this UI.
+        /// </summary>
+        /// <param name="matter">The matter to remove.</param>
+        /// <param name="quantity">The amount of that matter to remove (optional, defaults to 1).</param>
+        public void RemoveMatter(Matter matter, int quantity = 1)
+        {
+            if (matter != null && this.displayedMatter.ContainsKey(matter))
+            {
+                MatterIcon matterIcon = this.displayedMatter[matter];
+
+                if (matterIcon.DisplayedQuantity - quantity <= 0)
+                {
+                    GameObject.Destroy(matterIcon.gameObject);
+                    this.displayedMatter.Remove(matter);
+                }
+                else
+                    matterIcon.SetDisplay(matterIcon.DisplayedMatter, matterIcon.DisplayedQuantity - quantity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MatterIcon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Underconnected
+{
+    /// <summary>
+    /// Represents a matter icon UI element.
+    /// </summary>
+    public class MatterIcon : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] Matter matterToDisplay = null;
+        [SerializeField] int quantityToDisplay = 1;
+
+        [Header("References")]
+        [SerializeField] Image matterIcon;
+        [SerializeField] GameObject quantityGameObject;
+        [SerializeField] TextMeshProUGUI quantityText;
+
+
+        /// <summary>
+        /// The currently displayed matter.
+        /// </summary>
+        public Matter DisplayedMatter => this.matterToDisplay;
+        /// <summary>
+        /// The currently displayed quantity.
+        /// </summary>
+        public int DisplayedQuantity => this.quantityToDisplay;
+
+
+        private void Awake() => this.SetDisplay(this.matterToDisplay, this.quantityToDisplay);
+
+
+        /// <summary>
+        /// Sets the matter and quantity to display.
+        /// </summary>
+        /// <param name="matter">The matter to display.</param>
+        /// <param name="quantity">The quantity to display.</param>
+        public void SetDisplay(Matter matter, int quantity)
+        {
+            this.matterToDisplay = matter;
+            this.quantityToDisplay = quantity;
+
+            this.matterIcon.enabled = matter != null;
+            this.matterIcon.sprite = matter != null ? matter.GetIcon() : null;
+            this.quantityGameObject.SetActive(quantity > 1);
+            this.quantityText.text = quantity.ToString();
+        }
+    }
+}
