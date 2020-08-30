b836866837bcbd1b428ab2078a6919e561831cbb
pantAlex <banpa@web.de>
2020-08-30 12:14:38 +0200

    Fix filled stars not turning to empty stars + Improve stars display

--- Assets/Scripts/LevelFinishedUI.cs
+++ Assets/Scripts/LevelFinishedUI.cs
@@ -93,21 +93,11 @@ namespace Underconnected
         /// <summary>
         /// Sets the amount of reached stars to display.
         /// </summary>
-        /// <param name="score">The amount of stars.</param>
-        public void SetStars(int score)
+        /// <param name="amount">The amount of stars.</param>
+        public void SetStars(int amount)
         {
-            if (score >= 100) //placeholder points
-            {
-                stars[0].sprite = filledStar;
-                if (score >= 200)
-                {
-                    stars[1].sprite = filledStar;
-                    if (score >= 300)
-                    {
-                        stars[2].sprite = filledStar;
-                    }
-                }
-            }
+            for (int i = 0; i < this.stars.Length; i++)
+                this.stars[i].sprite = i < amount ? this.filledStar : this.emptyStar;
         }
 
         /// <summary>
